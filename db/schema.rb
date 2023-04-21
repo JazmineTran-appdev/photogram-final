934402a80ae1950f47ea5c1e7ea7d950b8b6b042
Jazmine Tran <jazminetra00@gmail.com>
2023-04-21 16:08:24 +0000

    created and migrated photos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_21_155643) do
+ActiveRecord::Schema.define(version: 2023_04_21_160715) do
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.string "image"
+    t.integer "owner_id"
+    t.integer "comments_count"
+    t.integer "likes_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
