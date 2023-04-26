046bbe5577989e51d995c7b4f644da797428d81b
Jazmine Tran <jazminetra00@gmail.com>
2023-04-26 15:51:59 +0000

    cannot view private user's page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,18 @@ class UsersController < ApplicationController
 
     @the_user = matching_user.first
 
-    render({ :template => "users/show.html.erb" })
+    
+    @matching_request = FollowRequest.where({ :recipient_id => @the_user.id, :sender_id => @current_user.id }).first
+    
+    if @the_user.private?
+      if @the_user.followers.include?(@current_user) && @matching_request.status == "accepted"
+        render({ :template => "users/show.html.erb" })
+      else
+        redirect_to("/", { :alert => "You're not authorized for that." })
+      end
+    else
+        render({ :template => "users/show.html.erb" })
+    end
   end
 
   def show_liked
